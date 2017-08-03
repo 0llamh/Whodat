cc573ea5cf8bc0e0ec0d425727e1f61933370a59
Gage <ghg13c@my.fsu.edu>
2017-08-02 21:15:45 -0400

    Implemented the OnClickListener for the first button and went ahead and passed intents like normal. This should help with further testing.

--- app/src/main/java/com/example/ollamh/whodat/MainActivity.java
+++ app/src/main/java/com/example/ollamh/whodat/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.ollamh.whodat;
 
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,19 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //henlo
         String [] seasons={"1","2","3","4","5","6","7"};
         String [] episodes={"1","2","3","4","5","6","7","8","9","10"};
 
-        Spinner spinner = (Spinner) findViewById(R.id.season_spinner);
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, seasons );
+        final Spinner spinner = (Spinner) findViewById(R.id.season_spinner);
+        final ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, seasons );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        Spinner spinner2 = (Spinner) findViewById(R.id.episode_spinner);
+        final Spinner spinner2 = (Spinner) findViewById(R.id.episode_spinner);
         ArrayAdapter<String> adapter2= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, episodes );
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
+        Button search = (Button) findViewById(R.id.button);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent= new Intent(getApplicationContext(), Selection.class);
+                final String season = spinner.getSelectedItem().toString();
+                final String episode = spinner2.getSelectedItem().toString();
+                intent.putExtra("episode", episode);
+                intent.putExtra("season", season);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/ollamh/whodat/Selection.java
+++ app/src/main/java/com/example/ollamh/whodat/Selection.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 public class Selection extends AppCompatActivity {
 
-    List<Character> cList;
+    List<Character> cList = new ArrayList<>();
 
 
 
@@ -16,6 +18,9 @@ public class Selection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+        Bundle extras = getIntent().getExtras();
+        String episode = extras.getString("episode");
+        String season = extras.getString("season");
 
         RecyclerView rvCharacters = (RecyclerView) findViewById(R.id.rvcharacters);
 
